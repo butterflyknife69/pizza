20fd7fc4340fd1e6c3767097e1ff47b8dcbad495
Igor <kishmashine@gmail.com>
2024-01-08 21:24:48 +0200

    get pizzas from the backend (fetch)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,22 @@ import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
 import pizzas from './assets/pizzas.json'
 
+
+
+
 function App() {
+
+  fetch('https://659c37a4d565feee2dacac9d.mockapi.io/items')
+  .then((res)=>{
+    return res.json()
+    })
+    .then(json =>{
+    console.log(json);
+  })
+
+
+
+
   return (
     <div className="wrapper">
       <Header />
