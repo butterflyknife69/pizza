53318acf8782da414ffde15f61147c9a2aa87ef7
Igor <kishmashine@gmail.com>
2024-01-19 00:38:19 +0200

    refactoring

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,55 +1,3 @@
-// import React from "react";
-// import Category from "./Category/Category";
-// import PizzaBlock from "./PizzaBlock/PizzaBlock";
-// import Skeleton from "./PizzaBlock/Skeleton";
-// import Sort from "./Sort/Sort";
-
-
-// const Content = () => {
-//   const [items, setItems] = React.useState([])
-//   const [isLoading, setIdLoading] = React.useState(true)
-//   const [categoryId, setCategoryId] = React.useState(0)
-//   const [sortType, setSortType] = React.useState({
-//     name: 'популярности', sortProperty: 'rating'
-//   })
-
-//   React.useEffect(() => {
-//     setIdLoading(true);
-//     fetch(`https://659c37a4d565feee2dacac9d.mockapi.io/items?${categoryId > 0 ? `category=${categoryId}` : ''
-//       }&sortBy=${sortType.sortProperty}&order=desc`
-//     )
-//       .then((res) => res.json())
-//       .then((data) => {
-//         const receivedItems = Array.isArray(data) ? data : [];
-//         setItems(receivedItems);
-//         setIdLoading(false);
-//       })
-//     window.scrollTo(0, 0);
-//   }, [categoryId, sortType])
-
-
-
-//   return (
-//     <div className="container">
-//       <div className="content__top">
-//         <Category value={categoryId} onClickCategory={(i) => setCategoryId(i)} />
-//         <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
-//       </div>
-//       <h2 className="content__title">Все пиццы</h2>
-//       <div className="content__items">
-//         {
-//           isLoading
-//             ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
-//             : items.map(p => <PizzaBlock key={p.id} {...p} />)
-//         }
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Content;
-
-
 import React from "react";
 import Category from "./Category/Category";
 import PizzaBlock from "./PizzaBlock/PizzaBlock";
@@ -66,7 +14,12 @@ const Content = ({ searchValue }) => {
 
   React.useEffect(() => {
     setIdLoading(true);
-    fetch(`https://659c37a4d565feee2dacac9d.mockapi.io/items?${categoryId > 0 ? `category=${categoryId}` : ''}&sortBy=${sortType.sortProperty}&order=desc`)
+    const sortBy = sortType.sortProperty.replace('-', '')
+    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc'
+    const category = categoryId > 0 ? `category=${categoryId}` : ''
+    const search = searchValue ? `&search=${searchValue}` : ''
+
+    fetch(`https://659c37a4d565feee2dacac9d.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}${search} `,)
       .then((res) => res.json())
       .then((data) => {
         const receivedItems = Array.isArray(data) ? data : [];
@@ -79,10 +32,10 @@ const Content = ({ searchValue }) => {
       });
 
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue]);
 
-  const pizzas = items.filter(obj =>{
-    if(obj.title.toLowerCase().includes(searchValue.toLowerCase())){
+  const pizzas = items.filter(obj => {
+    if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
       return true
     }
     return false
